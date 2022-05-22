7d2cee875dd1e3dcd77ef24cfbff2165ab31fb26
Victor Fedorov <vpfedorov@bk.ru>
2022-05-22 17:13:34 +0300

    styles

--- src/components/VotingList/VotingCard/VotingCard.js
+++ src/components/VotingList/VotingCard/VotingCard.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import './VotingCard.css';
+
 const VotingCard = ({ voting }) => {
 
     return (
-        <div>
-            <Link to={`${voting.id}`} >
+        <div className="voting-card">
+            <Link to={`${voting.id}`} className="voting-card__link">
                 <h3> { voting.title } </h3>
+                <p className="voting-card__description"> { voting.description } </p>
+
+                <i className="fa-solid fa-timer"></i>
+                <p className="voting-card__deadline"> { voting.deadline } </p>
+
             </Link>
-            <p> { voting.description } </p>
-            <p> { voting.deadline } </p>
         </div>
     );
 };
