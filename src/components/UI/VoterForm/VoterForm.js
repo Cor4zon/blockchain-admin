9f49dcce0b17d7979a3fb9277c65b94724c8d41c
Victor Fedorov <vpfedorov@bk.ru>
2022-06-01 10:42:34 +0300

    Adding voter by action card

--- src/components/UI/VoterForm/VoterForm.js
+++ src/components/UI/VoterForm/VoterForm.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import APIClient from "../../../services/APIClient";
 
 import "./VoterForm.css";
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 
-const VoterForm = ({ voting }) => {
+const VoterForm = () => {
+    const [ votingsList, setVotingList ] = useState([]);
     const [ pubkey, setPubkey ] = useState("");
+    const [ voting, setVoting ] = useState("");
     const client = new APIClient();
 
     const pubkeyChangeHandler = (event) => {
@@ -14,13 +17,46 @@ const VoterForm = ({ voting }) => {
     const addVoterHandler = (event) => {
         event.preventDefault();
         client.addVoter(pubkey, voting).then(() => {
+            console.log(`pubkey: ${pubkey}, voting: ${voting}`);
             console.log('voter added');
         });
+    }
+
+    useEffect(() => {
+        const votingsList = [];
+
+        client.fetchVotings().then((result) => {
+            for (let voting of result.data) {
+                votingsList.push([voting.title, voting.id]);
+            }
+
+            setVotingList([...votingsList]);
+        })
+    }, []);
+
+    const selectVotingHandler = (event) => {
+        setVoting(event.target.value);
     }
 
     return (
         <>
             <form className="voter-form">
+                <FormControl fullWidth>
+                    <InputLabel id="select-label">Voting</InputLabel>
+                    <Select
+                        id="selected-voting"
+                        value={voting}
+                        label="Voting"
+                        onChange={selectVotingHandler}
+                    >
+                        {
+                            votingsList.map((item, index) => {
+                                return <MenuItem key={index} value={item[1]}> { item[0] } </MenuItem>;
+                            })
+                        }
+                    </Select>
+                </FormControl>
+
                 <label>
                     <input type="text" name="name" onChange={ pubkeyChangeHandler } placeholder="Voter public key"/>
                 </label>
